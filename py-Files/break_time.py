27b91ede216465462a16606ca9f49efb3fc6beb2
Pepar Hugo <peparhugo@gmail.com>
2016-10-15 10:13:26 -0600

    added break_time.py file to repo

---
+++ py-Files/break_time.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# In[4]:
+
+import webbrowser
+import time
+
+
+# In[9]:
+
+x=10
+for n in range(1,3):
+    time.sleep(x)
+    webbrowser.open("https://www.netflix.com/title/80077977")
+
+
+# In[6]:
+
+
+
+
+# In[ ]:
+
+
+
